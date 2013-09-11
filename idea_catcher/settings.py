38ce1c2995e51a9437de0f127e1b213e274eb5ea
julian25 <julian25@gmail.com>
2013-09-11 13:52:07 -0400

    julianWork

--- idea_catcher/settings.py
+++ idea_catcher/settings.py
@@ -11,12 +11,12 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'idea_catcher',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': '',
         'PASSWORD': '',
-        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'HOST': 'localhost',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '',                      # Set to empty string for default.
     }
 }
@@ -111,6 +111,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    "/Users/juliangindi/Desktop/code/idea_catcher/idea_catcher/templates"
 )
 
 INSTALLED_APPS = (
@@ -120,10 +121,12 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'south',
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
+    'app',
 )
 
 # A sample logging configuration. The only tangible logging
--- idea_catcher/urls.py
+++ idea_catcher/urls.py
@@ -1,4 +1,6 @@
 from django.conf.urls import patterns, include, url
+from app import views
+from app.views import idea_list, new_idea
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -6,6 +8,8 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
     # Examples:
+    url(r'^list/$', idea_list),
+    url(r'^new/$', new_idea),
     # url(r'^$', 'idea_catcher.views.home', name='home'),
     # url(r'^idea_catcher/', include('idea_catcher.foo.urls')),
 
