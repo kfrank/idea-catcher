1b2bab143015d515ebfd04932a3da177d48f7288
julian25 <julian25@gmail.com>
2013-09-11 14:11:58 -0400

    added random record get

--- app/views.py
+++ app/views.py
@@ -13,9 +13,14 @@ def new_idea(request):
     if request.method == 'GET':
         return render(request, 'form.html')
     elif request.method == 'POST':
-        idea_text =  request.POST['idea_text']
+        idea_text = request.POST['idea_text']
         if idea_text:
             idea = models.Idea(text=idea_text) 
             idea.save()
             return HttpResponse("Success")
 
+def random(request):
+    results = {}
+    results["record"] = models.Idea.objects.order_by('?')[0].text
+    results["response"] = models.Responses.objects.order_by('?')[0].text
+    return HttpResponse(json.dumps(results))
--- idea_catcher/urls.py
+++ idea_catcher/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import patterns, include, url
 from app import views
-from app.views import idea_list, new_idea
+from app.views import idea_list, new_idea, random
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -10,8 +10,7 @@ urlpatterns = patterns('',
     # Examples:
     url(r'^list/$', idea_list),
     url(r'^new/$', new_idea),
-    # url(r'^$', 'idea_catcher.views.home', name='home'),
-    # url(r'^idea_catcher/', include('idea_catcher.foo.urls')),
+    url(r'^random/$', random),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
