78b06efb72dd81ff4c78c0c44c2f2be5eb258018
Klare Frank <klaredelune@gmail.com>
2013-09-12 09:44:04 -0400

    Added front-end

---
+++ idea_catcher/assets/j/functions-ck.js
@@ -0,0 +1,1 @@
+$(document).ready(function(){$(".get").click(function(){$("body").removeClass("give-active");$("body").addClass("get-active");$(".give h1").addClass("entypo-right-open-big")});$(".give").click(function(){$("body").removeClass("get-active");$("body").addClass("give-active");$(".get h1").addClass("entypo-left-open-big")})});
